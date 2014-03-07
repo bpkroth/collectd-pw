e759adc79f77da29059de1522a2bacfce59621ea
Brian Kroth <bpkroth@gmail.com>
2014-03-07 14:10:52 -0600

    try again

--- src/curl_xml.c
+++ src/curl_xml.c
@@ -30,6 +30,10 @@
 #include <libxml/xpath.h>
 #include <libxml/xpathInternals.h>
 
+#if HAVE_LIBGCRYPT
+#include <gcrypt.h>
+#endif
+
 #include <curl/curl.h>
 
 #define CX_DEFAULT_HOST "localhost"
@@ -1034,9 +1038,20 @@ static int cx_config (oconfig_item_t *ci) /* {{{ */
 
 static int cx_init (void) /* {{{ */
 {
+#ifdef HAVE_GCRYPT
+  /* http://lists.gnupg.org/pipermail/gcrypt-devel/2003-August/000458.html
+   * Because you can't know in a library whether another library has
+   * already initialized the library */
+  if (gcry_control (GCRYCTL_ANY_INITIALIZATION_P))
+    return;
+
+  gcry_check_version (NULL); /* before calling any other functions */
+  gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
+  gcry_control (GCRYCTL_DISABLE_SECMEM);
+  gcry_control (GCRYCTL_INITIALIZATION_FINISHED);
+#endif
   /* Call this while collectd is still single-threaded to avoid
    * initialization issues in libgcrypt. */
-  gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
   curl_global_init (CURL_GLOBAL_SSL);
   return (0);
 } /* }}} int cx_init */
