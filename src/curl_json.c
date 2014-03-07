925ddaab993b53a46ce2bd6892ab0f9e25cdb402
Brian Kroth <bpkroth@gmail.com>
2014-03-07 15:44:58 -0600

    Revert "also inform gcrypt that we're threaded, since at least squeeze's version of libgnutls doesn't appear to do that for us"
    This reverts commit 5bf4ebdbefa8764646a1ef2628ffa3d64f99db2d.

--- src/curl_json.c
+++ src/curl_json.c
@@ -960,7 +960,6 @@ static int cj_init (void) /* {{{ */
 {
   /* Call this while collectd is still single-threaded to avoid
    * initialization issues in libgcrypt. */
-  gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
   curl_global_init (CURL_GLOBAL_SSL);
   return (0);
 } /* }}} int cj_init */
